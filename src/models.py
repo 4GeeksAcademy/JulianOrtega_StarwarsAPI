2d0ba9939a168bc55a5763a5800582a50b926566
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-05-07 14:29:30 +0000

    Corrección del modelo para que People_id y Planets_id en Favorites sean opcionales y aceptar favoritos individuales

--- src/models.py
+++ src/models.py
@@ -1,107 +1,113 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer, Enum, ForeignKey
+from sqlalchemy import ForeignKey, String, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List
+from typing import List, Optional
 
 db = SQLAlchemy()
+
+user_has_planets = db.Table(
+    'User_has_Planets',
+    db.Column('User_id', db.Integer, ForeignKey('User.id'), primary_key=True),
+    db.Column('Planets_id', db.Integer, ForeignKey('Planets.id'), primary_key=True)
+)
+
+user_has_people = db.Table(
+    'User_has_People',
+    db.Column('User_id', db.Integer, ForeignKey('User.id'), primary_key=True),
+    db.Column('People_id', db.Integer, ForeignKey('People.id'), primary_key=True)
+)
 
 class User(db.Model):
     __tablename__ = 'User'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
-    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
-    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
-    email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True)
+    user_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password: Mapped[Optional[str]] = mapped_column(String(30))
 
-    posts: Mapped[List["Post"]] = relationship(back_populates="author")
-    comments: Mapped[List["Comment"]] = relationship(back_populates="author")
-    followers: Mapped[List["Follower"]] = relationship(
-        foreign_keys="[Follower.user_to_id]", back_populates="followed"
-    )
-    following: Mapped[List["Follower"]] = relationship(
-        foreign_keys="[Follower.user_from_id]", back_populates="follower"
-    )
+    planets: Mapped[List["Planets"]] = relationship(secondary=user_has_planets, back_populates="users")
+    people: Mapped[List["People"]] = relationship(secondary=user_has_people, back_populates="users")
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
-            "username": self.username,
-            "firstname": self.firstname,
-            "lastname": self.lastname,
-            "email": self.email,
+            "user_name": self.user_name,
+            "email": self.email
         }
 
-class Follower(db.Model):
-    __tablename__ = 'Follower'
+class Planets(db.Model):
+    __tablename__ = 'Planets'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_from_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
-    user_to_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[Optional[str]] = mapped_column(String(45))
+    diameter: Mapped[Optional[int]] = mapped_column()
+    rotation_period: Mapped[Optional[str]] = mapped_column(String(45))
+    terrain: Mapped[Optional[str]] = mapped_column(String(45))
+    gravity: Mapped[Optional[int]] = mapped_column()
+    orbital_period: Mapped[Optional[str]] = mapped_column(String(45))
+    population: Mapped[Optional[str]] = mapped_column(String(45))
+    climate: Mapped[Optional[str]] = mapped_column(String(45))
+    surface_water: Mapped[Optional[str]] = mapped_column(String(45))
 
-    follower: Mapped["User"] = relationship(
-        back_populates="following", foreign_keys=[user_from_id]
-    )
-    followed: Mapped["User"] = relationship(
-        back_populates="followers", foreign_keys=[user_to_id]
-    )
+    users: Mapped[List["User"]] = relationship(secondary=user_has_planets, back_populates="planets")
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="planet")
 
     def serialize(self):
         return {
             "id": self.id,
-            "user_from_id": self.user_from_id,
-            "user_to_id": self.user_to_id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "terrain": self.terrain,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+            "climate": self.climate,
+            "surface_water": self.surface_water
         }
 
-class Post(db.Model):
-    __tablename__ = 'Post'
+class People(db.Model):
+    __tablename__ = 'People'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(45), nullable=False)
+    gender: Mapped[str] = mapped_column(String(45), nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(45), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(45), nullable=False)
+    height: Mapped[str] = mapped_column(String(45), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(45), nullable=False)
 
-    author: Mapped["User"] = relationship(back_populates="posts")
-    comments: Mapped[List["Comment"]] = relationship(back_populates="post")
-    media: Mapped[List["Media"]] = relationship(back_populates="post")
+    users: Mapped[List["User"]] = relationship(secondary=user_has_people, back_populates="people")
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="people")
 
     def serialize(self):
         return {
             "id": self.id,
-            "user_id": self.user_id,
+            "name": self.name,
+            "gender": self.gender,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "eye_color": self.eye_color
         }
 
-class Comment(db.Model):
-    __tablename__ = 'Comment'
+class Favorites(db.Model):
+    __tablename__ = 'Favorites'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    commenttext: Mapped[str] = mapped_column(String(200), nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     User_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
-    Post_id: Mapped[int] = mapped_column(ForeignKey('Post.id'), nullable=False)
+    People_id: Mapped[Optional[int]] = mapped_column(ForeignKey('People.id'))
+    Planets_id: Mapped[Optional[int]] = mapped_column(ForeignKey('Planets.id'))
 
-    author: Mapped["User"] = relationship(back_populates="comments")
-    post: Mapped["Post"] = relationship(back_populates="comments")
-
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    people: Mapped[Optional["People"]] = relationship(back_populates="favorites")
+    planet: Mapped[Optional["Planets"]] = relationship(back_populates="favorites")
+    
     def serialize(self):
         return {
             "id": self.id,
-            "commenttext": self.commenttext,
             "User_id": self.User_id,
-            "Post_id": self.Post_id,
-        }
-
-class Media(db.Model):
-    __tablename__ = 'Media'
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    type: Mapped[str] = mapped_column(Enum('1', '2', '3', name='media_type'), nullable=False)
-    url: Mapped[str] = mapped_column(String(120), nullable=True)
-    Post_id: Mapped[int] = mapped_column(ForeignKey('Post.id'), nullable=False)
-
-    post: Mapped["Post"] = relationship(back_populates="media")
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "type": self.type,
-            "url": self.url,
-            "Post_id": self.Post_id,
+            "People_id": self.People_id,
+            "Planets_id": self.Planets_id
         }
